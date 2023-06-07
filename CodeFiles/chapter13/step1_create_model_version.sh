aeb2ef615f735ba59f7af539170a8c50202065e8
Phil Bates <74272212+batesph@users.noreply.github.com>
2023-06-07 16:18:02 -0400

    Update step1_create_model_version.sh

--- CodeFiles/chapter13/step1_create_model_version.sh
+++ CodeFiles/chapter13/step1_create_model_version.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 ./generate_show_model_sql.sh  'chapter7_regressionmodel.predict_ticket_price_auto'
 ./show_model.sh
+aws s3 cp model_output.txt s3://<your-s3-bucket>>
 ./prep_create_model.sh
 ./generate_create_model_version_sql.sh
 ./execute_create_model_version.sh                                         
