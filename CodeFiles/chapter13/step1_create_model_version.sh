6cc781f192c74c0c54a69f461b2152b96ed625e4
Phil Bates <74272212+batesph@users.noreply.github.com>
2023-06-07 15:29:00 -0400

    Update step1_create_model_version.sh

--- CodeFiles/chapter13/step1_create_model_version.sh
+++ CodeFiles/chapter13/step1_create_model_version.sh
@@ -2,3 +2,5 @@
 ./generate_show_model_sql.sh  'chapter7_regressionmodel.predict_ticket_price_auto'
 ./show_model.sh
 ./prep_create_model.sh
+./generate_create_model_version_sql.sh
+./execute_create_model_version.sh                                         
