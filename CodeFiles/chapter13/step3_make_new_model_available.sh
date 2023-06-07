57c8d1f7c9f1d50969f9f06edf02d987470060a3
Phil Bates <74272212+batesph@users.noreply.github.com>
2023-06-07 15:58:43 -0400

    Create step3_make_new_model_available.sh

---
+++ CodeFiles/chapter13/step3_make_new_model_available.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+isready=0
+
+while [ $isready != 1 ]
+do
+   echo 'model state ' $isready
+   ./call_model_ready_sql.sh
+   isready=$(grep '1' model_status.output)
+   echo 'model state ' $isready
+   if [ $isready = 1 ]
+   then echo 'model is ready' $isready 
+   else echo 'getting ready to sleep' sleep 1800
+   fi
+done
+echo 'creating new model ' 
+./create_model.sh
