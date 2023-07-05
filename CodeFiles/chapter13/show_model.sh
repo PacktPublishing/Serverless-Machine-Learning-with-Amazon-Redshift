ba41513e80a9f844b9a9c0468f6c1b7a6e3bed3d
Phil Bates <74272212+batesph@users.noreply.github.com>
2023-07-05 09:55:44 -0400

    Update show_model.sh

--- CodeFiles/chapter13/show_model.sh
+++ CodeFiles/chapter13/show_model.sh
@@ -1,1 +1,1 @@
-export PGPASSWORD=Awsuserxxx ;psql -h default.999999999999.us-east-1.redshift-serverless.amazonaws.com -p 5439 -U admin -d dev --output=create_model.txt -t  --file=show_model.sql
+rsql -D redshiftuser --output=create_model.txt -t  --file=show_model.sql
