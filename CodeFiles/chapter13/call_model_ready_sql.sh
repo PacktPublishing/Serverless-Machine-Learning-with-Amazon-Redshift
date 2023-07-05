69fa0c4c6cc549723ddb14e2a0818af03bbc107f
Phil Bates <74272212+batesph@users.noreply.github.com>
2023-07-05 10:10:02 -0400

    Delete call_model_ready_sql.sh

--- CodeFiles/chapter13/call_model_ready_sql.sh
+++
@@ -1,1 +0,0 @@
-export PGPASSWORD=Awsuser123 ;psql -h default.999999999999.us-east-1.redshift-serverless.amazonaws.com -p 5439 -U admin -d dev --output=model_status.output -t  --file=check_model_status.sql
